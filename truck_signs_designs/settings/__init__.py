dada5c991c04ae7b008d6a30862caa7561a6bde4
BenjaminTietz <mail@benjamin-tietz.com>
2025-03-26 17:16:07 +0100

    chore: __init__.py & simple_env_config.env reset to initial state exculde DJANGO_SETTINGS_MODULE_MODE variable

--- truck_signs_designs/settings/__init__.py
+++ truck_signs_designs/settings/__init__.py
@@ -1,10 +1,1 @@
-import os
-
-env_setting = os.environ.get('DJANGO_SETTINGS_MODULE_MODE', 'test_docker')
-
-if env_setting == 'production':
-    from .production import *
-elif env_setting == 'dev':
-    from .dev import *
-else:
-    from .test_docker import *
+from .test_docker import *
